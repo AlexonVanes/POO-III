bb5ad2982bedab1abc77439ee4172e6dc72c7d4f
Alexon Vanes <alexon.vanes.ifpinhais@gmail.com>
2023-08-21 14:59:12 -0300

    AgendaPOO

---
+++ src/app/view/detalhar/detalhar.page.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import Contato from 'src/app/model/entities/Contato';
+import { ContatoService } from 'src/app/model/services/contato.service';
+
+@Component({
+  selector: 'app-detalhar',
+  templateUrl: './detalhar.page.html',
+  styleUrls: ['./detalhar.page.scss'],
+})
+export class DetalharPage implements OnInit {
+  contato: Contato;
+  indice: number;
+  nome: string;
+  telefone: number;
+  email: string;
+
+  constructor(private actRoute: ActivatedRoute, private contatoService: ContatoService) { }
+
+  ngOnInit() {
+    this.actRoute.params.subscribe((parametros) => {
+      if(parametros["indice"]){
+        this.indice = parametros["indice"];
+        this.contato =
+        this.contatoService.obterPorIndice(this.indice);
+      }
+    })
+    this.nome = this.contato.nome;
+    this.telefone = this.contato.telefone;
+    this.email = this.contato.email;
+    console.log(this.contato);
+  }
+
+}
